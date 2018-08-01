ac764d173437e6ace968f86d9f2bac699b9d24a8
Guygolpur <guygolpur@se.shenkar.ac.il>
2018-08-01 14:58:17 +0300

    bookStore first commit

---
+++ Database.h
@@ -0,0 +1,42 @@
+/*Guy*/
+
+#ifndef _DB_H
+#define _DB_H
+
+#include <stdio.h>
+
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace sql;
+using namespace std;
+#define DB_NAME "book_store"
+#define DB_HOST "tcp://127.0.0.1/"
+#define DB_PORT 3306
+#define DB_USER "root"
+#define DB_PASS "Guy308353523"
+
+class Database {
+private:
+	// singleton instance
+	static Database *instance;
+
+	Driver *driver;
+	ConnectOptionsMap connection_properties;
+
+	// private CTOR (singleton)
+	Database();
+public:
+	static Database& getInstance();
+
+	Connection *getConnection();
+
+	virtual ~Database() {}
+};
+
+#endif /* Database_hpp */
